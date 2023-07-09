9c1508e08de0783f31fba0ce446e5958ade7ab9b
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2023-07-09 13:41:43 +0100

    added more colormaps

--- metalmaps/colors.py
+++ metalmaps/colors.py
@@ -20,12 +20,36 @@ blues_pills = hex_to_rgb(
 ["3D0B68","4A136F","521F77","F9A3A5","E58039","F9D1AB","FFD724","10843B","0D759B","51668C"]
         )
 
+cosmos_factory = hex_to_rgb(
+#  ["750712","B21F2B","D22841","7693B5","3763AF","DAF9FA","C3C3C7","9A3919","2F2C32","EFA517"]
+["750712","B21F2B","7693B5","3763AF","DAF9FA","C3C3C7","9A3919","EFA517","344B73","2F2C32"]
+        )
+
+deep_purple_in_rock = hex_to_rgb(
+
+["97866F","485244","7A7866","C7C5B9","142225","5E8893","4189AB"]        )
+
+dethalbum = hex_to_rgb(
+["12191D","16222C","122C3A","083847","517F92","8FC2D3","F1F5F7","415B6B","142732"]
+        )
+
+from_mars_to_sirius = hex_to_rgb(
+["E9E3D4","EFE9DE","E5DED1","DDC7BA","AA968B","C5BBAC","A09689","110E0A",]
+        )
+
+la_woman = hex_to_rgb(
+["76202B","81232B","89232E","3F2D16","BDA32C","FFFE44","FFF3BE"]
+        )
 london_calling = hex_to_rgb(
 ["130b0a","2e2724","1b9045","c8c0b0","989989","6b625a","d86a7d"]
         )
 
 master_of_puppets = hex_to_rgb(
 ["160907","2D0509","4C2415","989395","C6C2BE","E1CFB8","CF9262","EB9A00","CE5B0C","880E0A"]
+        )
+
+made_in_japan = hex_to_rgb(
+["9B7D4B","F1C36C","E3435B","D27C66","825F4C","835D5E","403B47","272532","F9D654"]
         )
 
 meteora = hex_to_rgb(
@@ -53,3 +77,8 @@ paranoid = hex_to_rgb(
 ride_the_lightning = hex_to_rgb(
 ["27233D","151040","22206C","423D74","5E5E89","869ACC","298CF1","82C7F6","EFF1F7"]
         )
+
+ziggy_stardust = hex_to_rgb(
+["08071B","0C1746","2E3442","564438","9B684E","DAA34C","004814","818B5B","527090","515D87"]
+        )
+
